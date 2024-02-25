cc7a7eb5634857eb1896d4668288aeb63f6aeb4f
Washinton-Ap <161253127+Washinton-Ap@users.noreply.github.com>
2024-02-25 16:45:00 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                //EJERCICIO: Calcular el area y perimetro de la circulo
+
+
+                // Definicion de variables y constantes
+                double radio, area, peri;
+
+                //Ingreso de datos
+                Console.WriteLine("CALCULO DEL ÁREA Y PERIMETRO DEL CIRCULO");
+                Console.Write("Ingrese Radio :");
+                radio = double.Parse(Console.ReadLine());
+
+                //Calculos
+
+                area = 3.1416 * Math.Pow(radio, 2);
+                peri = 2 * Math.PI * radio;
+
+                //mostrar resultados
+                Console.WriteLine("AREA      = {0}", area);
+                Console.WriteLine("PERIMETRO = {0}", peri);
+
+                Console.ReadKey();
+        }
+    }
+}
+
+   
